7b4b96432fc19f7ef40f70f4e72e5056d7505e0d
Bruno Ferreira <bmibferreira@gmail.com>
2018-07-04 17:23:37 +0100

    Update cenas.scala

--- cenas.scala
+++ cenas.scala
@@ -1,21 +1,23 @@
 object Thang2 {
-val bla = 666
-val blaa = 667
-val blaaa = 668
-val blaaaa = 669
-val blaaaaa = 670
-val blaaaaaa = 671
-val blaaaaaaa = 672
-val blaaaaaaaa = 673
-val blaaaaaaaaa = 674
-val blaaaaaaaaaa = 675
-val ble = 676
-val blee = 677
-val bleee = 678
-val bleeee = 679
-val bleeeee = 680
-val bleeeeee = 681
-val bleeeeeee = 682
-val bleeeeeeee = 683
-var xpto = 554
+  
+  private var pp = "xpto"  
+  val bla = 666
+  val blaa = 667
+  val blaaa = 668
+  val blaaaa = 669
+  val blaaaaa = 670
+  val blaaaaaa = 671
+  val blaaaaaaa = 672
+  val blaaaaaaaa = 673
+  val blaaaaaaaaa = 674
+  val blaaaaaaaaaa = 675
+  val ble = 676
+  val blee = 677
+  val bleee = 678
+  val bleeee = 679
+  val bleeeee = 680
+  val bleeeeee = 681
+  val bleeeeeee = 682
+  val bleeeeeeee = 683
+  var xpto = 554
 }
